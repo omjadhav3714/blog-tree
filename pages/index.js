18255cdc0147486b2fb59bc19973f81de8fc75dc
user <omjadhav963@gmail.com>
2022-06-19 16:21:16 +0530

    dynamic detail page + comments done

--- pages/index.js
+++ pages/index.js
@@ -10,18 +10,15 @@ export default function Home({ posts }) {
       <Hero />
       <div className="container mx-auto px-10 mb-8">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-          <div className='lg:col-span-8 col-span-1'>
+          <div className="lg:col-span-8 col-span-1">
             {posts.map((post, index) => (
-              <div>
-                <PostCard post={post.node} key={post.title} />
-              </div>
+              <PostCard key={index} post={post.node} />
             ))}
           </div>
           <div className="lg:col-span-4 col-span-1">
             <div className="lg:sticky relative top-8">
               <PostWidget />
               <Categories />
-
             </div>
           </div>
         </div>
